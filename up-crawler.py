b87bea864c108e81a3c54d09f199bc24c9809af0
David Sterry <davids@exchb.com>
2015-12-08 05:53:12 +0000

    crawler gets description, owner, name, fixes updating

--- up-crawler.py
+++ up-crawler.py
@@ -14,6 +14,16 @@ headers = {
     'User-Agent': "up API crawler/1",
 }
 
+def update_value(url, data, key):
+    u = connection.cursor()
+    sql = "UPDATE service SET `%s`=? where url=?" % key
+    if key in data:
+        value = data[key]
+        u.execute(sql, (value, url))
+    elif isinstance(data, list) and key in data[0]:
+        value = data[0][key]
+        u.execute(sql, (value, url))
+
 def update_service(url):
     us = connection.cursor()
     res = us.execute("SELECT owner, name from service where url = ? limit 1", (url,))
@@ -22,6 +32,7 @@ def update_service(url):
         (owner, name) = row
         count = count + 1
 
+    # service not in our db
     if count == 0:
         return 
 
@@ -36,26 +47,34 @@ def update_service(url):
     except Exception as e:
         print('%s error - %s' % (url, e)) 
         
+    # service is not responding
     if answer is None:
         return
 
     # try to parse as json
+    data = {}
     try:
         data = json.loads(answer.text)
-        if 'name' in data:
-            name = data['name']
-        elif 'name' in data[0]:
-            name = data[0]['name']
-        un = connection.cursor()
-        un.execute("UPDATE service SET name=? where url=?", (name, url))
     except:
         if code == 200:    
             print("code 200")
-            ur = connection.cursor()
-            ur.execute("UPDATE service SET response=?, updated=datetime('now') where url=?", (answer.text, url))
+
+    ur = connection.cursor()
+    ur.execute("UPDATE service SET response=?, updated=datetime('now') where url=?", (answer.text, url))
+
+    # parse out the info file
+    #if 'name' in data:
+    #    name = data['name']
+    #elif 'name' in data[0]:
+    #    name = data[0]['name']
+    #un = connection.cursor()
+    #un.execute("UPDATE service SET name=? where url=?", (name, url))
+
+    update_value(url, data, "name")
+    update_value(url, data, "description")
+    update_value(url, data, "owner")
 
     print('%s - %s' % (name, url))
-    print("\nResponse:\n" + answer.text)
     
 if len(sys.argv) > 1:
     print("Trying just one.")
